247b9fcabafe16606b13595ba46d76100767a151
stijn <stijn1c@gmail.com>
2022-06-19 16:16:23 +0200

    Tauri init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,12 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+fn main() {
+  let context = tauri::generate_context!();
+  tauri::Builder::default()
+    .menu(tauri::Menu::os_default(&context.package_info().name))
+    .run(context)
+    .expect("error while running tauri application");
+}
