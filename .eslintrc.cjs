a9e212e5f36022f5749f608007a616e51a186c47
Stijn-B <stijn1c@gmail.com>
2025-06-04 11:14:57 +0200

    small linting fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,13 @@
 module.exports = {
 	root: true,
 	extends: ['eslint:recommended', 'prettier'],
-	ignorePatterns: ['*.svelte'],
+	ignorePatterns: [
+		'src-tauri/target/**',
+		'src-tauri/gen/**',
+		'.svelte-kit/**',
+		'build/**',
+		'node_modules/**'
+	],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
